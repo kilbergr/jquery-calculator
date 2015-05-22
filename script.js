5fe8fff1da7db686f9a1a30de22aaf3df61e44de
kilbergr <rekilberg@gmail.com>
2015-05-21 20:44:28 -0700

    index.html

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ $(function(){
 		add: function(a, b){return(a+b)},
 		sub: function(a, b){return(a-b)},
 		mult: function(a, b){return(a*b)},
-		divi: function(a, b){return(a/b)};
+		divide: function(a, b){return(a/b)}
 	}
 
 	$("#button-container").click(function(e){
@@ -44,23 +44,22 @@ $(function(){
 					total = operators.sub(totalArr[0], totalArr[2]);
 					$("#screen").append("p").html(total);
 				}
+				if(totalArr[1]=="÷") {
+					total = operators.divide(totalArr[0], totalArr[2]);
+					$("#screen").append("p").html(total);
+				}
 				if(totalArr[1]==="x"){
 					total = operators.mult(totalArr[0], totalArr[2]);
 					$("#screen").append("p").html(total);
 				}
-				else if(totalArr[1]==="÷") {
-					total = operators.divi(totalArr[0], totalArr[2]);
-					$("#screen").append("p").html(total);
-					console.log(total);
-				}
+				
+					
 					leftArr = [];
 					rightArr =[];
 					totalArr = [];
 					leftVal = 0;
 					rightVal = 0;
 					total = 0;
-
-
 			}
 			else{
 				$("#screen").append(opValue).clone();
@@ -84,7 +83,6 @@ $(function(){
 			}
 
 		}	
-		console.log(totalArr);
 
 	});
 });
