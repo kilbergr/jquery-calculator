0006eb90206eb80b30e85008871306795be69a6f
kilbergr <rekilberg@gmail.com>
2015-05-21 20:33:55 -0700

    index.html

--- script.js
+++ script.js
@@ -1,37 +1,90 @@
+var reset = function(){
+	var	leftArr = [];
+	var rightArr =[];
+	var totalArr = [];
+	var leftVal = 0;
+	var rightVal = 0;
+
+	};
+
 $(function(){
+
+	var	leftArr = [];
+	var rightArr =[];
+	var totalArr = [];
+	var leftVal = 0;
+	var rightVal = 0;
+	var total = 0;
+
 	var operators = {
 		add: function(a, b){return(a+b)},
 		sub: function(a, b){return(a-b)},
 		mult: function(a, b){return(a*b)},
-		div: function(a, b){return(a/b)}
+		divi: function(a, b){return(a/b)};
 	}
 
-	var total = "";
 	$("#button-container").click(function(e){
 		if(e.target.id==="cancel"){
 			$("#screen").append("p").html("");
+			leftArr = [];
+			rightArr =[];
+			totalArr = [];
+			leftVal = 0;
+			rightVal = 0;
 			}
 		
 		else if ($(e.target).hasClass("operator")) {
 			var opValue = $(e.target).text();
 			if(opValue==="="){
-				if(innerOp==="+"){
-					add(left, clickValue1);
+				if(totalArr[1]==="+"){
+					total = operators.add(totalArr[0], totalArr[2]);
+					$("#screen").append("p").html(total);
 				}
+				if(totalArr[1]==="-"){
+					total = operators.sub(totalArr[0], totalArr[2]);
+					$("#screen").append("p").html(total);
+				}
+				if(totalArr[1]==="x"){
+					total = operators.mult(totalArr[0], totalArr[2]);
+					$("#screen").append("p").html(total);
+				}
+				else if(totalArr[1]==="÷") {
+					total = operators.divi(totalArr[0], totalArr[2]);
+					$("#screen").append("p").html(total);
+					console.log(total);
+				}
+					leftArr = [];
+					rightArr =[];
+					totalArr = [];
+					leftVal = 0;
+					rightVal = 0;
+					total = 0;
 
-			//$("#screen").append("p").html(opValue);
+
 			}
 			else{
-				var clickValue2 = $("#screen").append(opValue).clone();
-				var innerOp = opValue;	
-				console.log(innerOp);
-				
+				$("#screen").append(opValue).clone();
+				totalArr[1] = opValue;	
+				console.log(opValue);
 			}
 		}
 
 		else {
-			var digitValue = $(e.target).html();
-			var clickValue1 = $("#screen").append(digitValue).clone();
+			if(totalArr[1]){
+				var digitValue = $(e.target).html();
+				$("#screen").append(digitValue).clone();
+				rightArr.push(digitValue);
+				totalArr[2] = parseInt(rightArr.join(""), 10);
+			}
+			else {
+				var digitValue = $(e.target).html();
+				$("#screen").append(digitValue).clone();
+				leftArr.push(digitValue);
+				totalArr[0] = parseInt(leftArr.join(""), 10);
+			}
+
 		}	
+		console.log(totalArr);
+
 	});
 });
